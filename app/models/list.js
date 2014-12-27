5afe14db2a16fae3cd25059efa76a48960f0bfa5
jmosesman <jmosesman@ou.edu>
2014-12-27 11:39:28 -0600

    Generating and showing an index of lists

--- app/models/list.js
+++ app/models/list.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var List = DS.Model.extend({
   title: DS.attr('string')
 });
+
+List.reopenClass({
+  FIXTURES: [
+    { id: 1, title: 'List 1' },
+    { id: 2, title: 'List 2' }
+  ]
+});
+
+export default List
