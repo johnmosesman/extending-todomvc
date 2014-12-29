aee9d8bd5b2f4d5484296fdee4457a3d8a6ab52e
jmosesman <jmosesman@ou.edu>
2014-12-29 15:54:12 -0600

    Replace FixtureAdapter with LocalStorageAdapter

--- app/models/list.js
+++ app/models/list.js
@@ -1,14 +1,5 @@
 import DS from 'ember-data';
 
-var List = DS.Model.extend({
+export default DS.Model.extend({
   title: DS.attr('string')
 });
-
-List.reopenClass({
-  FIXTURES: [
-    { id: 1, title: 'List 1' },
-    { id: 2, title: 'List 2' }
-  ]
-});
-
-export default List;
