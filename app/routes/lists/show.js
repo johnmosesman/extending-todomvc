d41631a5d459f708ba7e4ba8c91fc89ce243c2b7
jmosesman <jmosesman@ou.edu>
2014-12-29 16:34:04 -0600

    Deleting a list.

--- app/routes/lists/show.js
+++ app/routes/lists/show.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('list', params.list_id);
+  },
+
+  actions: {
+    deleteList: function() {
+      var list = this.modelFor(this.routeName);
+      list.destroyRecord();
+
+      this.transitionTo('lists');
+    }
   }
 });
