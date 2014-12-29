65a475332dfd0ad37fdec0d5e645bbcc6528e828
jmosesman <jmosesman@ou.edu>
2014-12-29 16:04:03 -0600

    Creating a new list and basic styling

--- app/routes/lists.js
+++ app/routes/lists.js
@@ -3,5 +3,25 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('list');
+  },
+
+  actions: {
+    createList: function() {
+      var newListTitle = this.controllerFor('lists').get('newListTitle');
+
+      if (Ember.isBlank(newListTitle)) { return false; }
+
+      var list = this.store.createRecord('list', {
+        title: newListTitle
+      });
+
+      this.controllerFor('lists').set('newListTitle', '');
+
+      var _this = this;
+      list.save().then(function(list) {
+        console.log(list.id);
+        _this.transitionTo('lists.show', list);
+      });
+    }
   }
 });
