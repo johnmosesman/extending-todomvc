75b99fa624cd13690b3e52358128a7c3a822eb61
jmosesman <jmosesman@ou.edu>
2015-01-09 21:48:45 -0600

    Generating todos and relationships

--- app/models/todo.js
+++ app/models/todo.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
+  isCompleted: DS.attr('boolean'),
 
-  todos: DS.hasMany('todo', {async: true})
+  list: DS.belongsTo('list', {async: true})
 });
