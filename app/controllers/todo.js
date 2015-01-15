c4b2f57c8e95f9fdc45cf4572b473ccfb460b2d0
jmosesman <jmosesman@ou.edu>
2015-01-14 21:44:10 -0600

    Marking todos as completed

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -3,6 +3,19 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   isEditing: false,
 
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if (value === undefined) {
+      return model.get('isCompleted');
+    }
+    else {
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted'),
+
   actions: {
     editTitle: function() {
       this.set('isEditing', true);
@@ -19,6 +32,6 @@ export default Ember.Controller.extend({
       else {
         model.save();
       }
-    }
+    },
   }
 });
